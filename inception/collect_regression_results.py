20f4e606e510caa7d7ef4cf638dece2361b6de38
Lucas Bechberger <lbechberger@uos.de>
2018-02-07 10:04:38 +0100

    more regression configurations, improved usability

--- inception/collect_regression_results.py
+++ inception/collect_regression_results.py
@@ -10,11 +10,20 @@ Created on Wed Feb  7 08:24:49 2018
 import os
 
 with open("regression/summary.csv", 'w') as out_file:
-    out_file.write("config, train_rmse, test_rmse\n")
+    out_file.write("config,train_rmse,test_rmse\n")
     file_names = [fn for fn in os.listdir("regression/") if not fn.endswith("csv")]
     for file_name in file_names:
+        mean_train = 0
+        mean_test = 0
+        counter = 0
         with open("regression/{0}".format(file_name), 'r') as in_file:
             for line in in_file:
                 if len(line) > 0:
-                    out_file.write("{0},{1}".format(file_name, line))
+                    [train, test] = line.split(',')
+                    counter += 1
+                    mean_train += float(train)
+                    mean_test += float(test)
+        mean_train = (1.0 * mean_train) / counter
+        mean_test = (1.0 * mean_test) / counter
+        out_file.write("{0},{1},{2}\n".format(file_name, mean_train, mean_test))
             
