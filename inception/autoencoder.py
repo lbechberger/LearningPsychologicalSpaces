fixed RMSE computation

inception/autoencoder.py
@@ -20,7 +20,7 @@ flags.DEFINE_integer('num_steps', 2000, 'Number of optimization steps.')
 flags.DEFINE_integer('batch_size', 64, 'Batch size used during training.')
 flags.DEFINE_float('keep_prob', 0.8, 'Keep probability for dropout.')
 flags.DEFINE_float('alpha', 0.5, 'Influence of L2 loss.')
-flags.DEFINE_float('learning_rate', 0.01, 'Learning rate.')
+flags.DEFINE_float('learning_rate', 0.001, 'Learning rate.')
 
 FLAGS = flags.FLAGS
 
@@ -48,7 +48,7 @@ tf_data = tf.placeholder(tf.float32, shape=[None, FLAGS.features_size])
 hidden = tf.nn.relu(tf.matmul(tf_data, weights_encoder) + bias_encoder)
 dropout = tf.nn.dropout(hidden, FLAGS.keep_prob)
 prediction = tf.matmul(dropout, weights_decoder) + bias_decoder
-mse = tf.reduce_mean(tf.square(prediction - tf_data))    
+mse = tf.reduce_mean(tf.reduce_sum(tf.square(prediction - tf_data), axis=1))    
 
 global_step = tf.Variable(0)
 loss = mse + FLAGS.alpha * (tf.nn.l2_loss(weights_encoder) + tf.nn.l2_loss(bias_encoder) + tf.nn.l2_loss(weights_decoder) + tf.nn.l2_loss(bias_decoder))

inception/baselines.py
@@ -8,11 +8,10 @@ Created on Wed Feb  7 10:56:46 2018
 """
 
 
-import os, sys
+import sys
 import tensorflow as tf
 import pickle
 from math import sqrt
-from random import shuffle
 from configparser import RawConfigParser
 import fcntl
 
@@ -44,12 +43,12 @@ tf_labels = tf.placeholder(tf.float32, shape=[None, options['space_size']])
 
 # defining the zero baseline network
 zero_output = tf.zeros(shape=(1, options['space_size']))
-zero_mse = tf.reduce_mean(tf.square(zero_output - tf_labels))
+zero_mse = tf.reduce_mean(tf.reduce_sum(tf.square(zero_output - tf_labels), axis=1))
 
 # defining the random baseline network
 normal_dist = tf.contrib.distributions.MultivariateNormalDiag(tf.zeros(shape=(1,options['space_size'])), [0.4]*options['space_size'])
 random_output = normal_dist.sample()
-random_mse = tf.reduce_mean(tf.square(random_output - tf_labels))    
+random_mse = tf.reduce_mean(tf.reduce_sum(tf.square(random_output - tf_labels), axis=1))    
 
 real_squared_train_errors_zero = []
 real_squared_test_errors_zero = []

inception/linear_regression.py
@@ -20,11 +20,11 @@ options['features_file'] = 'features/images'
 options['targets_file'] = 'features/targets-4d'
 options['features_size'] = 2048
 options['space_size'] = 4
-options['num_steps'] = 200
+options['num_steps'] = 2000
 options['batch_size'] = 64
 options['keep_prob'] = 0.8
 options['alpha'] = 5.0             # influence of L2 loss
-options['learning_rate'] = 0.01
+options['learning_rate'] = 0.003
 
 
 config_name = sys.argv[1]
@@ -60,7 +60,7 @@ tf_labels = tf.placeholder(tf.float32, shape=[None, options['space_size']])
 
 dropout = tf.nn.dropout(tf_data, options['keep_prob'])
 prediction = tf.matmul(dropout, weights) + bias
-mse = tf.reduce_mean(tf.square(prediction - tf_labels))    
+mse = tf.reduce_mean(tf.reduce_sum(tf.square(prediction - tf_labels), axis=1))  
 
 global_step = tf.Variable(0)
 loss = mse + options['alpha'] * (tf.nn.l2_loss(weights) + tf.nn.l2_loss(bias))
@@ -102,7 +102,8 @@ for test_image in input_data.keys():
             batch_labels = real_labels_train[offset:(offset + options['batch_size'])]
             
             feed_dict = {tf_data : batch_data, tf_labels : batch_labels}
-            _, l = session.run([optimizer, loss], feed_dict = feed_dict)    
+            _, l = session.run([optimizer, loss], feed_dict = feed_dict)  
+            #print("Loss in step {0}: {1}".format(step, l))
             if isnan(l):
                 print("Loss NaN in step {0}!".format(step))
                 break