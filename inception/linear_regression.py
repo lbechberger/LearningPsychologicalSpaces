a7c9e5ad1a8e464ed2ac3404449ea815f0d6faf3
Lucas Bechberger <lbechberger@uos.de>
2018-02-16 10:18:56 +0100

    final pinning attempt

--- inception/linear_regression.py
+++ inception/linear_regression.py
@@ -15,42 +15,43 @@ from random import shuffle
 from configparser import RawConfigParser
 import fcntl
 
-options = {}
-options['features_file'] = 'features/images'
-options['targets_file'] = 'features/targets-4d'
-options['features_size'] = 2048
-options['space_size'] = 4
-options['num_steps'] = 2000
-options['batch_size'] = 64
-options['keep_prob'] = 0.8
-options['alpha'] = 5.0             # influence of L2 loss
-options['learning_rate'] = 0.003
-
-config_name = sys.argv[1]
-config = RawConfigParser(options)
-config.read("grid_search.cfg")
-
-if config.has_section(config_name):
-    options['features_file'] = config.get(config_name, 'features_file')
-    options['targets_file'] = config.get(config_name, 'targets_file')
-    options['features_size'] = config.getint(config_name, 'features_size')
-    options['space_size'] = config.getint(config_name, 'space_size')
-    options['num_steps'] = config.getint(config_name, 'num_steps')
-    options['batch_size'] = config.getint(config_name, 'batch_size')
-    options['keep_prob'] = config.getfloat(config_name, 'keep_prob')
-    options['alpha'] = config.getfloat(config_name, 'alpha')
-    options['learning_rate'] = config.getfloat(config_name, 'learning_rate')
-
-try:
-    input_data = pickle.load(open(options['features_file'], 'rb'))
-    targets_data = pickle.load(open(options['targets_file'], 'rb'))
-except Exception:
-    print("Cannot read input data. Aborting.")
-    sys.exit(0)
-
 with tf.device('/cpu:0'):
-    real_targets = targets_data['targets']
-    shuffled_targets = targets_data['shuffled']
+    
+    options = {}
+    options['features_file'] = 'features/images'
+    options['targets_file'] = 'features/targets-4d'
+    options['features_size'] = 2048
+    options['space_size'] = 4
+    options['num_steps'] = 2000
+    options['batch_size'] = 64
+    options['keep_prob'] = 0.8
+    options['alpha'] = 5.0             # influence of L2 loss
+    options['learning_rate'] = 0.003
+    
+    config_name = sys.argv[1]
+    config = RawConfigParser(options)
+    config.read("grid_search.cfg")
+    
+    if config.has_section(config_name):
+        options['features_file'] = config.get(config_name, 'features_file')
+        options['targets_file'] = config.get(config_name, 'targets_file')
+        options['features_size'] = config.getint(config_name, 'features_size')
+        options['space_size'] = config.getint(config_name, 'space_size')
+        options['num_steps'] = config.getint(config_name, 'num_steps')
+        options['batch_size'] = config.getint(config_name, 'batch_size')
+        options['keep_prob'] = config.getfloat(config_name, 'keep_prob')
+        options['alpha'] = config.getfloat(config_name, 'alpha')
+        options['learning_rate'] = config.getfloat(config_name, 'learning_rate')
+    
+    try:
+        input_data = pickle.load(open(options['features_file'], 'rb'))
+        targets_data = pickle.load(open(options['targets_file'], 'rb'))
+    except Exception:
+        print("Cannot read input data. Aborting.")
+        sys.exit(0)
+    
+        real_targets = targets_data['targets']
+        shuffled_targets = targets_data['shuffled']
 
 # defining the linear regression network
 weights = tf.Variable(tf.truncated_normal([options['features_size'],options['space_size']]))
@@ -100,11 +101,12 @@ with tf.Session(config=config) as session:
         # first train real
         tf.global_variables_initializer().run()
         for step in range(options['num_steps']):
-            offset = (step * options['batch_size']) % (len(features_train) - options['batch_size'])
-            batch_data = features_train[offset:(offset + options['batch_size'])]
-            batch_labels = real_labels_train[offset:(offset + options['batch_size'])]
-            
-            feed_dict = {tf_data : batch_data, tf_labels : batch_labels}
+            with tf.device('/cpu:0'):
+                offset = (step * options['batch_size']) % (len(features_train) - options['batch_size'])
+                batch_data = features_train[offset:(offset + options['batch_size'])]
+                batch_labels = real_labels_train[offset:(offset + options['batch_size'])]
+                
+                feed_dict = {tf_data : batch_data, tf_labels : batch_labels}
             _, l = session.run([optimizer, loss], feed_dict = feed_dict)  
             #print("Loss in step {0}: {1}".format(step, l))
             if isnan(l):
@@ -119,11 +121,12 @@ with tf.Session(config=config) as session:
         # now train shuffled
         tf.global_variables_initializer().run()
         for step in range(options['num_steps']):
-            offset = (step * options['batch_size']) % (len(features_train) - options['batch_size'])
-            batch_data = features_train[offset:(offset + options['batch_size'])]
-            batch_labels = shuffled_labels_train[offset:(offset + options['batch_size'])]
-            
-            feed_dict = {tf_data : batch_data, tf_labels : batch_labels}
+            with tf.device('/cpu:0'):
+                offset = (step * options['batch_size']) % (len(features_train) - options['batch_size'])
+                batch_data = features_train[offset:(offset + options['batch_size'])]
+                batch_labels = shuffled_labels_train[offset:(offset + options['batch_size'])]
+                
+                feed_dict = {tf_data : batch_data, tf_labels : batch_labels}
             _, l = session.run([optimizer, loss], feed_dict = feed_dict)    
             if isnan(l):
                 print("Loss NaN in step {0}!".format(step))
