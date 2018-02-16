1e3ecf2261aedba69698ffc87ed3f2dd4c6d02d7
Lucas Bechberger <lbechberger@uos.de>
2018-02-16 10:18:56 +0100

    final pinning attempt

--- inception/linear_regression.py
+++ inception/linear_regression.py
@@ -15,42 +15,43 @@ from random import shuffle
 from configparser import RawConfigParser
 import fcntl
 
-options = {}
-options['features_file'] = 'features/images'
-options['targets_file'] = 'features/targets-4d'
-options['features_size'] = 2048
-options['space_size'] = 4
-options['num_steps'] = 2000
-options['batch_size'] = 64
-options['keep_prob'] = 0.8
-options['alpha'] = 5.0             # influence of L2 loss
-options['learning_rate'] = 0.003
-
-config_name = sys.argv[1]
-config = RawConfigParser(options)
-config.read("grid_search.cfg")
-
-if config.has_section(config_name):
-    options['features_file'] = config.get(config_name, 'features_file')
-    options['targets_file'] = config.get(config_name, 'targets_file')
-    options['features_size'] = config.getint(config_name, 'features_size')
-    options['space_size'] = config.getint(config_name, 'space_size')
-    options['num_steps'] = config.getint(config_name, 'num_steps')
-    options['batch_size'] = config.getint(config_name, 'batch_size')
-    options['keep_prob'] = config.getfloat(config_name, 'keep_prob')
-    options['alpha'] = config.getfloat(config_name, 'alpha')
-    options['learning_rate'] = config.getfloat(config_name, 'learning_rate')
-
-try:
-    input_data = pickle.load(open(options['features_file'], 'rb'))
-    targets_data = pickle.load(open(options['targets_file'], 'rb'))
-except Exception:
-    print("Cannot read input data. Aborting.")
-    sys.exit(0)
-
 with tf.device('/cpu:0'):
-    real_targets = targets_data['targets']
-    shuffled_targets = targets_data['shuffled']
+    
+    options = {}
+    options['features_file'] = 'features/images'
+    options['targets_file'] = 'features/targets-4d'
+    options['features_size'] = 2048
+    options['space_size'] = 4
+    options['num_steps'] = 2000
+    options['batch_size'] = 64
+    options['keep_prob'] = 0.8
+    options['alpha'] = 5.0             # influence of L2 loss
+    options['learning_rate'] = 0.003
+    
+    config_name = sys.argv[1]
+    config = RawConfigParser(options)
+    config.read("grid_search.cfg")
+    
+    if config.has_section(config_name):
+        options['features_file'] = config.get(config_name, 'features_file')
+        options['targets_file'] = config.get(config_name, 'targets_file')
+        options['features_size'] = config.getint(config_name, 'features_size')
+        options['space_size'] = config.getint(config_name, 'space_size')
+        options['num_steps'] = config.getint(config_name, 'num_steps')
+        options['batch_size'] = config.getint(config_name, 'batch_size')
+        options['keep_prob'] = config.getfloat(config_name, 'keep_prob')
+        options['alpha'] = config.getfloat(config_name, 'alpha')
+        options['learning_rate'] = config.getfloat(config_name, 'learning_rate')
+    
+    try:
+        input_data = pickle.load(open(options['features_file'], 'rb'))
+        targets_data = pickle.load(open(options['targets_file'], 'rb'))
+    except Exception:
+        print("Cannot read input data. Aborting.")
+        sys.exit(0)
+    
+        real_targets = targets_data['targets']
+        shuffled_targets = targets_data['shuffled']
 
 # defining the linear regression network
 weights = tf.Variable(tf.truncated_normal([options['features_size'],options['space_size']]))
