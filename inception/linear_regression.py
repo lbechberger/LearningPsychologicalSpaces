006c2123508bc3681ce6ced1257efaca87b46e1f
Lucas Bechberger <lbechberger@uos.de>
2018-02-16 10:31:35 +0100

    another indentation error

--- inception/linear_regression.py
+++ inception/linear_regression.py
@@ -48,8 +48,8 @@ except Exception:
     print("Cannot read input data. Aborting.")
     sys.exit(0)
 
-    real_targets = targets_data['targets']
-    shuffled_targets = targets_data['shuffled']
+real_targets = targets_data['targets']
+shuffled_targets = targets_data['shuffled']
 
 # defining the linear regression network
 weights = tf.Variable(tf.truncated_normal([options['features_size'],options['space_size']]))
