67f0758985eb37599ec06338644cd7a896390f42
Lucas Bechberger <lbechberger@uos.de>
2019-02-15 10:51:44 +0100

    added SMACOF to MDS.r

--- code/mds/mds.r
+++ code/mds/mds.r
@@ -3,9 +3,9 @@ if(!require(optparse)){
   install.packages("optparse")
   library(optparse)
 }
-if(!require(MASS)){
-  install.packages("MASS")
-  library(MASS)
+if(!require(smacof)){
+  install.packages("smacof")
+  library(smacof)
 }
 
 option_list = list(make_option(c("-d", "--distance_file"), type = "character", default = NULL, help = "path to distance file"),
@@ -16,7 +16,9 @@ option_list = list(make_option(c("-d", "--distance_file"), type = "character", d
                    make_option(c("-m", "--max_iter"), type = "integer", default = 1000, help = "maximum number of iterations for Kruskal algorithm"),
                    make_option(c("-s", "--seed"), type = "integer", default = NULL, help = "seed for the random number generator"),
                    make_option(c("-p", "--plot"), action = "store_true", default = FALSE, help = "plot the stress value"),
-                   make_option(c("--metric"), action = "store_true", default = FALSE, help = "use metric instead of nonmetric MDS"))
+                   make_option(c("--metric"), action = "store_true", default = FALSE, help = "use metric instead of nonmetric MDS"),
+                   make_option(c("--smacof"), action = "store_true", default = FALSE, help = "use SMACOF algorithm"))
+
 opt_parser = OptionParser(option_list = option_list)
 opt = parse_args(opt_parser)
 
@@ -36,59 +38,140 @@ num_stimuli = sqrt(length(dissimilarities))
 # Load item names
 item_names = read.csv(opt$items_file, header = FALSE)
 
+# collecting the stress for each of the spaces
 stress_by_dimension = list()
+
 if (opt$metric) {
-  # iterate over number of dimensions
-  for (num_dims in 1:opt$dims) {
-    # Perform Metric MDS
-    metric_mds_result = cmdscale(dissimilarities, k = num_dims)
+  # METRIC MDS
+  
+  if (opt$smacof) {
+    # use SMACOF algorithm
     
-    # write vectors to file
-    metric_output = cbind(item_names, metric_mds_result)
-    metric_output_file_name = paste0(opt$output_folder, num_dims, "D-vectors.csv")
-    write.table(metric_output, metric_output_file_name, sep = ',', row.names = FALSE, col.names = FALSE, quote = FALSE)
-    
-    # compute stress manually
-    distances = as.matrix(dist(metric_mds_result))
-    difference = distances - dissimilarities
-    squared_difference = difference * difference
-    stress = sum(squared_difference)
-    
-    # print stress, store it for later plotting
-    print(paste(num_dims, stress, sep=','), quote = FALSE)
-    stress_by_dimension = c(stress_by_dimension, stress)
-  }
-  plot_title = "Stress of metric MDS (Eigenvalue)"
-} else {
-  # iterate over number of dimensions
-  for (num_dims in 1:opt$dims) {
-    # local variables for keeping best configuration
-    best_stress = 1000 
-    points = NULL
-    # run nonmetric MDS multiple times
-    for (i in 1:opt$n_init) {
-      # new random configuration at each step
-      initial_config = matrix(rnorm(num_stimuli*num_dims), ncol = num_dims)
-      # run Kruskal's algorithm
-      kruskal_mds_result = isoMDS(dissimilarities, k = num_dims, y = initial_config, trace=FALSE, maxit = opt$max_iter)
-      # if better than before: store result
-      if (kruskal_mds_result$stress < best_stress) {
-        best_stress = kruskal_mds_result$stress
-        points = kruskal_mds_result$points
+    # iterate over number of dimensions
+    for (num_dims in 1:opt$dims) {
+      # local variables for keeping best configuration
+      best_stress = 1000 
+      points = NULL
+      # run metric SMACOF multiple times, keep best result
+      for (i in 1:opt$n_init) {
+        # new random configuration at each step
+        initial_config = matrix(rnorm(num_stimuli*num_dims), ncol = num_dims)
+        # run metric SMACOF algorithm
+        mds_result = smacofSym(dissimilarities, ndim = num_dims, init = initial_config, verbose=FALSE, itmax = opt$max_iter, type = "ratio")
+        # if better than before: store result
+        if (mds_result$stress < best_stress) {
+          best_stress = mds_result$stress
+          points = mds_result$conf
+        }
       }
+      
+      # save vectors in file
+      output = cbind(item_names, points)
+      output_file_name = paste0(opt$output_folder, num_dims, "D-vectors.csv")
+      write.table(output, output_file_name, sep = ',', row.names = FALSE, col.names = FALSE, quote = FALSE)
+      
+      # print stress and store it for plotting
+      print(paste(num_dims, best_stress, sep=','), quote = FALSE)
+      stress_by_dimension = c(stress_by_dimension, best_stress)
     }
     
-    # save vectors in file
-    nonmetric_output = cbind(item_names, points)
-    nonmetric_output_file_name = paste0(opt$output_folder, num_dims, "D-vectors.csv")
-    write.table(nonmetric_output, nonmetric_output_file_name, sep = ',', row.names = FALSE, col.names = FALSE, quote = FALSE)
+    plot_title = "Stress of metric MDS (SMACOF)"
+
+  } else {
+    # use classical MDS
     
-    # print stress and store it for plotting
-    print(paste(num_dims, best_stress, sep=','), quote = FALSE)
-    stress_by_dimension = c(stress_by_dimension, best_stress)
+    # iterate over number of dimensions
+    for (num_dims in 1:opt$dims) {
+      # Perform Metric MDS
+      metric_mds_result = cmdscale(dissimilarities, k = num_dims)
+      
+      # write vectors to file
+      metric_output = cbind(item_names, metric_mds_result)
+      metric_output_file_name = paste0(opt$output_folder, num_dims, "D-vectors.csv")
+      write.table(metric_output, metric_output_file_name, sep = ',', row.names = FALSE, col.names = FALSE, quote = FALSE)
+      
+      stress = stress0(dissimilarities, metric_mds_result, type = "ordinal")
+      
+      # print stress, store it for later plotting
+      print(paste(num_dims, stress, sep=','), quote = FALSE)
+      stress_by_dimension = c(stress_by_dimension, stress)
+    }
+    plot_title = "Stress of metric MDS (classical)"
   }
+} else {
+  # NONMETRIC MDS
   
-  plot_title = "Stress of nonmetric MDS (Kruskal)"
+  if (opt$smacof) {
+    # use SMACOF algorithm
+
+    # iterate over number of dimensions
+    for (num_dims in 1:opt$dims) {
+      # local variables for keeping best configuration
+      best_stress = 1000 
+      points = NULL
+      # run metric SMACOF multiple times, keep best result
+      for (i in 1:opt$n_init) {
+        # new random configuration at each step
+        initial_config = matrix(rnorm(num_stimuli*num_dims), ncol = num_dims)
+        # run metric SMACOF algorithm
+        mds_result = smacofSym(dissimilarities, ndim = num_dims, init = initial_config, verbose=FALSE, itmax = opt$max_iter, type = "ordinal")
+        # if better than before: store result
+        if (mds_result$stress < best_stress) {
+          best_stress = mds_result$stress
+          points = mds_result$conf
+        }
+      }
+      
+      # save vectors in file
+      output = cbind(item_names, points)
+      output_file_name = paste0(opt$output_folder, num_dims, "D-vectors.csv")
+      write.table(output, output_file_name, sep = ',', row.names = FALSE, col.names = FALSE, quote = FALSE)
+      
+      # print stress and store it for plotting
+      print(paste(num_dims, best_stress, sep=','), quote = FALSE)
+      stress_by_dimension = c(stress_by_dimension, best_stress)
+    }
+    
+    plot_title = "Stress of nonmetric MDS (SMACOF)"
+  } else {
+    # use Kruskal's algorithm
+    
+    if(!require(MASS)){
+      install.packages("MASS")
+      library(MASS)
+    }
+    # iterate over number of dimensions
+    for (num_dims in 1:opt$dims) {
+      # local variables for keeping best configuration
+      best_stress = 1000 
+      points = NULL
+      # run nonmetric MDS multiple times
+      for (i in 1:opt$n_init) {
+        # new random configuration at each step
+        initial_config = matrix(rnorm(num_stimuli*num_dims), ncol = num_dims)
+        # run Kruskal's algorithm
+        kruskal_mds_result = isoMDS(dissimilarities, k = num_dims, y = initial_config, trace=FALSE, maxit = opt$max_iter)
+        # if better than before: store result
+        current_stress = kruskal_mds_result$stress / 100
+        if (current_stress < best_stress) {
+          best_stress = current_stress
+          points = kruskal_mds_result$points
+        }
+      }
+      
+      # save vectors in file
+      nonmetric_output = cbind(item_names, points)
+      nonmetric_output_file_name = paste0(opt$output_folder, num_dims, "D-vectors.csv")
+      write.table(nonmetric_output, nonmetric_output_file_name, sep = ',', row.names = FALSE, col.names = FALSE, quote = FALSE)
+      
+      # print stress and store it for plotting
+      print(paste(num_dims, best_stress, sep=','), quote = FALSE)
+      stress_by_dimension = c(stress_by_dimension, best_stress)
+    }
+    
+    plot_title = "Stress of nonmetric MDS (Kruskal)"
+    
+  }
 }
 
 if (opt$plot) {
