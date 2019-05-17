6953b6ecf99a1e54e69a414c639672b70b970f17
Lucas Bechberger <lbechberger@uos.de>
2019-05-17 11:13:57 +0200

    renamed target spaces, prepared experiments 2 and 3

--- code/shell_scripts/NOUN/experiment_2.sh
+++ code/shell_scripts/NOUN/experiment_2.sh
@@ -5,35 +5,44 @@ echo 'experiment 2'
 # set up the directory structure
 echo '    setting up directory structure'
 rm -r -f data/NOUN/ML_results/experiment_2
-mkdir -p data/NOUN/ML_results/experiment_2
+mkdir -p data/NOUN/ML_results/experiment_2/classical data/NOUN/ML_results/experiment_2/Kruskal 
+mkdir -p data/NOUN/ML_results/experiment_2/metric_SMACOF data/NOUN/ML_results/experiment_2/nonmetric_SMACOF
 
-echo '    classical MDS'
-echo '        inception'
-echo '            baseline'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle classical_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_2/inception.csv -s 42 --zero
-echo '            linear'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle classical_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_2/inception.csv -s 42 --linear
-echo '            lasso 0.25'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle classical_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_2/inception.csv -s 42 --lasso 0.25
-echo '            lasso 0.5'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle classical_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_2/inception.csv -s 42 --lasso 0.5
-echo '            lasso 1.0'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle classical_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_2/inception.csv -s 42 --lasso 1.0
-echo '            lasso 2.0'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle classical_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_2/inception.csv -s 42 --lasso 2.0
+# declare some lists to make code below less repetitive 
+feature_sets=("inception") #TODO: add best image-based one
+baselines=("--zero --mean --normal --draw")
+regressors=("--linear --random_forest")
+lassos=("0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0")
+targets=("classical Kruskal metric_SMACOF nonmetric_SMACOF")
 
-echo '        reduced images: TODO'
-echo '            baseline'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle classical_4 data/NOUN/dataset/TODO.pickle data/NOUN/ML_results/experiment_2/inception.csv -s 42 --zero
-echo '            linear'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle classical_4 data/NOUN/dataset/TODO.pickle data/NOUN/ML_results/experiment_2/inception.csv -s 42 --linear
-echo '            lasso 0.25'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle classical_4 data/NOUN/dataset/TODO.pickle data/NOUN/ML_results/experiment_2/inception.csv -s 42 --lasso 0.25
-echo '            lasso 0.5'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle classical_4 data/NOUN/dataset/TODO.pickle data/NOUN/ML_results/experiment_2/inception.csv -s 42 --lasso 0.5
-echo '            lasso 1.0'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle classical_4 data/NOUN/dataset/TODO.pickle data/NOUN/ML_results/experiment_2/inception.csv -s 42 --lasso 1.0
-echo '            lasso 2.0'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle classical_4 data/NOUN/dataset/TODO.pickle data/NOUN/ML_results/experiment_2/inception.csv -s 42 --lasso 2.0
+for target in $targets
+do
+	echo '    $target'
+	# first compute the baselines: using a single feature set is sufficient as features are ignored anyways, don't need shuffled targets as results are same anyways
+	echo '        baselines'
+	for baseline in $baselines
+	do
+		echo "            $baseline"	
+		python code/regression/regression.py data/NOUN/dataset/targets.pickle "$target"'_4' data/NOUN/dataset/features_inception.pickle 'data/NOUN/ML_results/experiment_2/'"$target"'/baselines.csv' -s 42 $baseline
+	done
 
-# TODO: other 4D spaces (Kruskal, mSMACOF, nSMACOF)
+	# now compute the results for a linear regression and a random forest regression on all feature sets; also compute results on shuffled targets for comparison
+	echo '        regressors'
+	for feature_set in $feature_sets
+	do
+		echo "            $feature_set"
+		for regressor in $regressors
+		do
+			echo "                $regressor"
+			python code/regression/regression.py data/NOUN/dataset/targets.pickle "$target"'_4' 'data/NOUN/dataset/features_'"$feature_set"'.pickle' 'data/NOUN/ML_results/experiment_2/'"$target"'/'"$feature_set"'.csv' -s 42 --shuffled $regressor
+		done
+
+		for lasso in $lassos
+		do
+			echo "            lasso $lasso"
+			python code/regression/regression.py data/NOUN/dataset/targets.pickle "$target"'_4' 'data/NOUN/dataset/features_'"$feature_set"'.pickle' 'data/NOUN/ML_results/experiment_2/'"$target"'/'"$feature_set"'.csv' -s 42 --lasso $lasso
+		done
+
+	done
+done
+
--- code/shell_scripts/NOUN/experiment_3.sh
+++ code/shell_scripts/NOUN/experiment_3.sh
@@ -7,4 +7,43 @@ echo '    setting up directory structure'
 rm -r -f data/NOUN/ML_results/experiment_3
 mkdir -p data/NOUN/ML_results/experiment_3
 
+# declare some lists to make code below less repetitive 
+feature_sets=("inception") #TODO: add best image-based one
+baselines=("--zero --mean --normal --draw")
+regressors=("--linear --random_forest")
+lassos=("0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0")
+targets=("1 2 3 5 6 7 8 9 10")
+
+for target in $targets
+do
+	echo '    Kruskal_$target'
+	# first compute the baselines: using a single feature set is sufficient as features are ignored anyways, don't need shuffled targets as results are same anyways
+	echo '        baselines'
+	for baseline in $baselines
+	do
+		echo "            $baseline"	
+		python code/regression/regression.py data/NOUN/dataset/targets.pickle 'Kruskal_'"$target" data/NOUN/dataset/features_inception.pickle 'data/NOUN/ML_results/experiment_3/Kruskal_'"$target"'/baselines.csv' -s 42 $baseline
+	done
+
+	# now compute the results for the real regressions
+	echo '        regressors'
+	for feature_set in $feature_sets
+	do
+		echo "            $feature_set"
+		for regressor in $regressors
+		do
+			echo "                $regressor"
+			python code/regression/regression.py data/NOUN/dataset/targets.pickle "$target"'_4' 'data/NOUN/dataset/features_'"$feature_set"'.pickle' 'data/NOUN/ML_results/experiment_3/Kruskal_'"$target"'/'"$feature_set"'.csv' -s 42 $regressor
+		done
+
+		for lasso in $lassos
+		do
+			echo "            lasso $lasso"
+			python code/regression/regression.py data/NOUN/dataset/targets.pickle "$target"'_4' 'data/NOUN/dataset/features_'"$feature_set"'.pickle' 'data/NOUN/ML_results/experiment_3/Kruskal_'"$target"'/'"$feature_set"'.csv' -s 42 --lasso $lasso
+		done
+
+	done
+done
+
+
 # TODO: for each dimensionality in best MDS strategy: run best baseline, best pixel, inception
