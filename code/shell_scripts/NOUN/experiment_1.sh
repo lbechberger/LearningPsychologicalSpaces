3367774c520df7f67907afbb8cbf6d03037536df
Lucas Bechberger <lbechberger@uos.de>
2019-05-17 07:44:25 +0200

    updated structure of experiment 1

--- code/shell_scripts/NOUN/experiment_1.sh
+++ code/shell_scripts/NOUN/experiment_1.sh
@@ -2,109 +2,47 @@
 
 echo 'experiment 1'
 
+# declare some lists to make code below less repetitive 
+feature_sets=("inception image_min_7_g image_min_18_g image_min_12_rgb image_min_18_rgb")
+lasso_sets=("inception")
+baselines=("--zero --mean --normal --draw")
+regressors=("--linear --random_forest")
+lassos=("0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0")
+
 # set up the directory structure
 echo '    setting up directory structure'
 rm -r -f data/NOUN/ML_results/experiment_1
 mkdir -p data/NOUN/ML_results/experiment_1
 
-baselines=("--zero" "--mean" "--normal" "--draw")
-max=5
-
+# first compute the baselines: using a single feature set is sufficient as features are ignored anyways, don't need shuffled targets as results are same anyways
+echo '    baselines'
 for baseline in $baselines
 do
-	echo $baseline
-	#python -u code/mds/analyze_convexity.py 'data/Shapes/vectors/mean/Kruskal/'"$i"'D-vectors.csv' data/Shapes/raw_data/data.pickle $i -o data/Shapes/analysis/mean/Kruskal/convexity/convexities.csv -b -r 100 -s 42 > 'data/Shapes/analysis/mean/Kruskal/convexity/'"$i"'D-convexity.txt' &
+	echo "        $baseline"	
+	python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_1/inception.csv -s 42 $baseline
 done
 
+# now compute the results for a linear regression and a random forest regression on all feature sets; also compute results on shuffled targets for comparison
+echo '    regressors'
+for feature_set in $feature_sets
+do
+	echo "        $feature_set"
+	for regressor in $regressors
+	do
+		echo "            $regressor"
+		python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 'data/NOUN/dataset/features_'"$feature_set"'.pickle' 'data/NOUN/ML_results/experiment_1/'"$feature_set"'.csv' -s 42 --shuffled $regressor
+	done
+done
 
-# first use the inception features; here also once compute baselines (they are independent of the feature space, so recomputing them every time does not make too much sense)
-echo '    inception features'
-echo '        zero'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_1/inception.csv -s 42 --zero
-echo '        mean'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_1/inception.csv -s 42 --mean
-echo '        normal'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_1/inception.csv -s 42 --normal
-echo '        draw'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_1/inception.csv -s 42 --draw
-echo '        linear'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_1/inception.csv -s 42 --linear
-echo '        lasso 0.01'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_1/inception.csv -s 42 --lasso 0.01
-echo '        lasso 0.02'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_1/inception.csv -s 42 --lasso 0.02
-echo '        lasso 0.05'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_1/inception.csv -s 42 --lasso 0.05
-echo '        lasso 0.1'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_1/inception.csv -s 42 --lasso 0.1
-echo '        lasso 0.2'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_1/inception.csv -s 42 --lasso 0.2
-echo '        lasso 0.5'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_inception.pickle data/NOUN/ML_results/experiment_1/inception.csv -s 42 --lasso 0.5
-
-# for features based on reduced images: only do linear regression and lasso regression
-echo '    reduced images: min 7 grey'
-echo '        linear'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_7_g.pickle data/NOUN/ML_results/experiment_1/min_7_grey.csv -s 42 --linear
-echo '        lasso 0.01'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_7_g.pickle data/NOUN/ML_results/experiment_1/min_7_grey.csv -s 42 --lasso 0.01
-echo '        lasso 0.02'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_7_g.pickle data/NOUN/ML_results/experiment_1/min_7_grey.csv -s 42 --lasso 0.02
-echo '        lasso 0.05'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_7_g.pickle data/NOUN/ML_results/experiment_1/min_7_grey.csv -s 42 --lasso 0.05
-echo '        lasso 0.1'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_7_g.pickle data/NOUN/ML_results/experiment_1/min_7_grey.csv -s 42 --lasso 0.1
-echo '        lasso 0.2'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_7_g.pickle data/NOUN/ML_results/experiment_1/min_7_grey.csv -s 42 --lasso 0.2
-echo '        lasso 0.5'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_7_g.pickle data/NOUN/ML_results/experiment_1/min_7_grey.csv -s 42 --lasso 0.5
-
-echo '    reduced images: min 18 grey'
-echo '        linear'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_g.pickle data/NOUN/ML_results/experiment_1/min_18_grey.csv -s 42 --linear
-echo '        lasso 0.01'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_g.pickle data/NOUN/ML_results/experiment_1/min_18_grey.csv -s 42 --lasso 0.01
-echo '        lasso 0.02'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_g.pickle data/NOUN/ML_results/experiment_1/min_18_grey.csv -s 42 --lasso 0.02
-echo '        lasso 0.05'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_g.pickle data/NOUN/ML_results/experiment_1/min_18_grey.csv -s 42 --lasso 0.05
-echo '        lasso 0.1'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_g.pickle data/NOUN/ML_results/experiment_1/min_18_grey.csv -s 42 --lasso 0.1
-echo '        lasso 0.2'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_g.pickle data/NOUN/ML_results/experiment_1/min_18_grey.csv -s 42 --lasso 0.2
-echo '        lasso 0.5'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_g.pickle data/NOUN/ML_results/experiment_1/min_18_grey.csv -s 42 --lasso 0.5
-
-
-echo '    reduced images: min 12 rgb'
-echo '        linear'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_12_rgb.pickle data/NOUN/ML_results/experiment_1/min_12_rgb.csv -s 42 --linear
-echo '        lasso 0.01'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_12_rgb.pickle data/NOUN/ML_results/experiment_1/min_12_rgb.csv -s 42 --lasso 0.01
-echo '        lasso 0.02'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_12_rgb.pickle data/NOUN/ML_results/experiment_1/min_12_rgb.csv -s 42 --lasso 0.02
-echo '        lasso 0.05'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_12_rgb.pickle data/NOUN/ML_results/experiment_1/min_12_rgb.csv -s 42 --lasso 0.05
-echo '        lasso 0.1'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_12_rgb.pickle data/NOUN/ML_results/experiment_1/min_12_rgb.csv -s 42 --lasso 0.1
-echo '        lasso 0.2'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_12_rgb.pickle data/NOUN/ML_results/experiment_1/min_12_rgb.csv -s 42 --lasso 0.2
-echo '        lasso 0.5'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_12_rgb.pickle data/NOUN/ML_results/experiment_1/min_12_rgb.csv -s 42 --lasso 0.5
-
-echo '    reduced images: min 18 rgb'
-echo '        linear'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_rgb.pickle data/NOUN/ML_results/experiment_1/min_18_rgb.csv -s 42 --linear
-echo '        lasso 0.01'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_rgb.pickle data/NOUN/ML_results/experiment_1/min_18_rgb.csv -s 42 --lasso 0.01
-echo '        lasso 0.02'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_rgb.pickle data/NOUN/ML_results/experiment_1/min_18_rgb.csv -s 42 --lasso 0.02
-echo '        lasso 0.05'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_rgb.pickle data/NOUN/ML_results/experiment_1/min_18_rgb.csv -s 42 --lasso 0.05
-echo '        lasso 0.1'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_rgb.pickle data/NOUN/ML_results/experiment_1/min_18_rgb.csv -s 42 --lasso 0.1
-echo '        lasso 0.2'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_rgb.pickle data/NOUN/ML_results/experiment_1/min_18_rgb.csv -s 42 --lasso 0.2
-echo '        lasso 0.5'
-python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 data/NOUN/dataset/features_image_min_18_rgb.pickle data/NOUN/ML_results/experiment_1/min_18_rgb.csv -s 42 --lasso 0.5
+# finally do a grid search on the lasso regressor for the two selected feature sets
+echo '    lasso regressor'
+for feature_set in $lasso_sets
+do
+	echo "        $feature_set"
+	for lasso in $lassos
+	do
+		echo "            lasso $lasso"
+		python code/regression/regression.py data/NOUN/dataset/targets.pickle HorstHout_4 'data/NOUN/dataset/features_'"$feature_set"'.pickle' 'data/NOUN/ML_results/experiment_1/'"$feature_set"'.csv' -s 42 --lasso $lasso
+	done
+done
 
