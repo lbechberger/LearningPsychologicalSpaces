bebd69faa46c9e460844f9d1718b30a0bc61ca65
Lucas Bechberger <lbechberger@uos.de>
2020-02-04 12:12:30 +0100

    simplified clean_Shapes.sh

--- code/shell_scripts/clean_Shapes.sh
+++ code/shell_scripts/clean_Shapes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -r data/Shapes/mds/analysis data/Shapes/mds/similarities data/Shapes/mds/vectors data/Shapes/mds/visualizations data/Shapes/raw_data/preprocessed data/Shapes/mds/classification data/Shapes/mds/regression
+rm -r data/Shapes/mds/ data/Shapes/raw_data/preprocessed
