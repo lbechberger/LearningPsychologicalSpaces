two bug fixes for correlation computation (optimized weights)
- not adding information of first fold twice
- concatenating instead of averaging predictions and targets

code/util.py
@@ -148,14 +148,18 @@ def compute_correlations(vectors, dissimilarities, distance_function, n_folds = 
                 results_list = {}
                 for key, value in result.items():
                     results_list[key] = [value]
-            
-            for key, value in result.items():
-                results_list[key].append(value)
+            else:
+                for key, value in result.items():
+                    results_list[key].append(value)
             
         # average across folds
         overall_result = {}
         for key, value in results_list.items():
-            overall_result[key] = np.mean(results_list[key])
+            
+            if key == 'predictions' or key == 'targets':
+                overall_result[key] = np.concatenate(value, axis = 0)
+            else:
+                overall_result[key] = np.mean(value)
         
         overall_result['weights'] = np.mean(weights_list, axis = 0)        
         