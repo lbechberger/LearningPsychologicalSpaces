6756de733815cfe101568fc1905029c101165f7a
Lucas Bechberger <lbechberger@uos.de>
2020-03-08 11:33:18 +0100

    use minimum aggregation and no s&p noise in NOUN ML study

--- code/shell_scripts/NOUN/ml_setup.sh
+++ code/shell_scripts/NOUN/ml_setup.sh
@@ -10,7 +10,7 @@ echo 'preparing data set for machine learning'
 
 # create more artificial images
 echo '    augmentation'
-python -m code.ml.preprocessing.data_augmentation data/NOUN/images/ data/NOUN/ml/dataset/augmented 1000 -s 42 --flip_prob 0.0 --crop_size 0.05 --scale_min 0.9 --scale_max 1.1 --translation 0.1 --sp_noise_prob 0.01 --rotation_angle 15
+python -m code.ml.preprocessing.data_augmentation data/NOUN/images/ data/NOUN/ml/dataset/augmented 1000 -s 42 --flip_prob 0.0 --crop_size 0.05 --scale_min 0.9 --scale_max 1.1 --translation 0.1 --sp_noise_prob 0.0 --rotation_angle 15
 
 # collect regression targets
 echo '    regression targets'
@@ -21,8 +21,8 @@ echo '    feature extraction'
 echo '        ANN-based features'
 python -m code.ml.regression.ann_features /tmp/inception data/NOUN/ml/dataset/augmented data/NOUN/ml/dataset/features_ANN.pickle
 echo '        pixel-based features'
-python -m code.ml.regression.pixel_features data/NOUN/ml/dataset/augmented/ data/NOUN/ml/dataset/features_pixel_1875.pickle -b 12 -a mean
-python -m code.ml.regression.pixel_features data/NOUN/ml/dataset/augmented/ data/NOUN/ml/dataset/features_pixel_507.pickle -b 24 -a mean
+python -m code.ml.regression.pixel_features data/NOUN/ml/dataset/augmented/ data/NOUN/ml/dataset/features_pixel_1875.pickle -b 12 -a min
+python -m code.ml.regression.pixel_features data/NOUN/ml/dataset/augmented/ data/NOUN/ml/dataset/features_pixel_507.pickle -b 24 -a min
 
 # analyze feature spaces
 echo '    cluster analysis'
