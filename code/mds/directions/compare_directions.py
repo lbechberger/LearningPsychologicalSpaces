9ed733a24341e177b19ef1109006ca52fd63dbda
Lucas Bechberger <lbechberger@uos.de>
2020-09-24 15:53:07 +0200

    compare_directions.py takes into account baselines

--- code/mds/directions/compare_directions.py
+++ code/mds/directions/compare_directions.py
@@ -8,6 +8,7 @@ Created on Sat Feb  1 15:09:35 2020
 """
 
 import argparse, os, csv
+import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from itertools import combinations
 
@@ -17,7 +18,6 @@ parser.add_argument('n_dims', type = int, help = 'the maximal number of dimensio
 parser.add_argument('output_file', help = 'csv file for output')
 args = parser.parse_args()
 
-# one entry for each dimension; then split up into direction_name; then contain list of normal vectors
 direction_data = {}
 for dims in range(1, args.n_dims + 1):
     direction_data[dims] = {}
@@ -26,15 +26,22 @@ for dims in range(1, args.n_dims + 1):
 for file_name in os.listdir(args.input_folder):
     if file_name.endswith('.csv'):
         direction_name = file_name.split('.')[0]
-        for dims in range(1, args.n_dims + 1):
-            direction_data[dims][direction_name] = []
 
         with open(os.path.join(args.input_folder, file_name), 'r') as f_in:            
             reader = csv.DictReader(f_in)
             for row in reader:
                 dims = int(row['dims'])
+                data_source = row['data_source']
+                space_idx = row['space_idx']
+                if data_source not in direction_data[dims]:
+                    direction_data[dims][data_source] = {}
+                if space_idx not in direction_data[dims][data_source]:
+                    direction_data[dims][data_source][space_idx] = {}
+                if direction_name not in direction_data[dims][data_source][space_idx]:
+                    direction_data[dims][data_source][space_idx][direction_name] = []
+
                 vector = [float(row['d{0}'.format(d)]) for d in range(dims)]
-                direction_data[dims][direction_name].append(vector)
+                direction_data[dims][data_source][space_idx][direction_name].append(vector)
 
 output_dict = {}
 categories = []
@@ -45,51 +52,66 @@ for dims in range(1, args.n_dims + 1):
     dims_dict = direction_data[dims]
     output_dict[dims] = {}
     
-    # first look at within-similarity for each direction (should be high)
-    for direction_name, all_vectors in dims_dict.items():
-        sim_matrix = cosine_similarity(all_vectors)
+    # look at each data source separately
+    for data_source in dims_dict.keys():    
+
+        space_dict = dims_dict[data_source]
+        output_dict[dims][data_source] = {}
         
-        # only take into account entries above the diagonal
-        avg_cos_sim = 0
-        counter = 0
-        for i in range(len(all_vectors)):
-            for j in range(i, len(all_vectors)):
-                avg_cos_sim += sim_matrix[i][j]
-                counter += 1
-        avg_cos_sim /= counter
-        
-        output_dict[dims][direction_name] = avg_cos_sim
-        if dims == 1:
-            categories.append(direction_name)
-    
-    # now look at between-similarity for all pairs of directions (should be low)
-    for first_direction, second_direction in combinations(sorted(dims_dict.keys()), 2):
-        first_vectors = dims_dict[first_direction]
-        second_vectors = dims_dict[second_direction]
-        all_vectors = first_vectors + second_vectors
-        sim_matrix = cosine_similarity(all_vectors)
-        
-        # only take into account entries above the diagonal, ignoring within-similarities
-        avg_cos_sim = 0
-        counter = 0
-        for i in range(len(first_vectors)):
-            for j in range(len(first_vectors), len(first_vectors) + len(second_vectors)):
-                avg_cos_sim += sim_matrix[i][j]
-                counter += 1
-        avg_cos_sim /= counter
-        
-        category_name = '{0}-{1}'.format(first_direction, second_direction)
-        output_dict[dims][category_name] = avg_cos_sim
-        if dims == 1:
-            categories.append(category_name)
+        for space_idx in space_dict.keys():
+            
+            inner_dict = space_dict[space_idx]
+            
+            # first look at within-similarity for each direction (should be high)
+            for direction_name, all_vectors in inner_dict.items():
+                sim_matrix = cosine_similarity(all_vectors)
+                
+                # only take into account entries above the diagonal
+                avg_cos_sim = 0
+                counter = 0
+                for i in range(len(all_vectors)):
+                    for j in range(i, len(all_vectors)):
+                        avg_cos_sim += sim_matrix[i][j]
+                        counter += 1
+                avg_cos_sim /= counter
+                
+                if direction_name not in output_dict[dims][data_source]:
+                    output_dict[dims][data_source][direction_name] = []
+                output_dict[dims][data_source][direction_name].append(avg_cos_sim)
+                if direction_name not in categories:
+                    categories.append(direction_name)
+            
+            # now look at between-similarity for all pairs of directions (should be low)
+            for first_direction, second_direction in combinations(sorted(inner_dict.keys()), 2):
+                first_vectors = inner_dict[first_direction]
+                second_vectors = inner_dict[second_direction]
+                all_vectors = first_vectors + second_vectors
+                sim_matrix = cosine_similarity(all_vectors)
+                
+                # only take into account entries above the diagonal, ignoring within-similarities
+                avg_cos_sim = 0
+                counter = 0
+                for i in range(len(first_vectors)):
+                    for j in range(len(first_vectors), len(first_vectors) + len(second_vectors)):
+                        avg_cos_sim += sim_matrix[i][j]
+                        counter += 1
+                avg_cos_sim /= counter
+                
+                category_name = '{0}-{1}'.format(first_direction, second_direction)
+                if category_name not in output_dict[dims][data_source]:
+                    output_dict[dims][data_source][category_name] = []
+                output_dict[dims][data_source][category_name].append(avg_cos_sim)
+                if category_name not in categories:
+                    categories.append(category_name)
 
 with open(args.output_file, 'w') as f_out:
     # write headline
-    headline_items = ['dims'] + categories
+    headline_items = ['dims,data_source'] + categories
     f_out.write(','.join(headline_items))
     f_out.write('\n')
     
     for dims in range(1, args.n_dims + 1):
-        line_items = [dims] + [output_dict[dims][category_name] for category_name in categories]
-        f_out.write(','.join(map(lambda x: str(x), line_items)))
-        f_out.write('\n')
+        for data_source in sorted(output_dict[dims].keys()):
+            line_items = [dims, data_source] + [np.mean(output_dict[dims][data_source][category_name]) for category_name in categories]
+            f_out.write(','.join(map(lambda x: str(x), line_items)))
+            f_out.write('\n')
