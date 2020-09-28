632905eb0429f50c89ea51b9e5b57b5aea9fdf6f
Lucas Bechberger <lbechberger@uos.de>
2020-09-28 09:43:15 +0200

    updates for increased efficiency

--- code/shell_scripts/NOUN/correlation.sh
+++ code/shell_scripts/NOUN/correlation.sh
@@ -31,8 +31,9 @@ for space in $spaces
 do
 	echo '    '"$space"
 	# if precomputed distances exist: use them; if not: re-compute them
-	[ -f 'data/NOUN/mds/correlations/'"$space"'_distances.pickle' ] && vectors_flag='' || vectors_flag='-v data/NOUN/mds/vectors/'"$space"'/vectors.pickle -b data/NOUN/mds/correlations/baseline_vectors.pickle'
-	python -m code.mds.correlations.mds_correlations data/NOUN/mds/similarities/sim.pickle 'data/NOUN/mds/correlations/'"$space"'_distances.pickle' 'data/NOUN/mds/correlations/'"$space"'.csv' --n_max $dims $correlation_metrics -s 42 $vectors_flag &> 'data/NOUN/mds/correlations/'"$space"'-log.txt' &
+	[ -f 'data/NOUN/mds/correlations/'"$space"'_distances.pickle' ] && vectors_flag='' || vectors_flag='-v data/NOUN/mds/vectors/'"$space"'/vectors.pickle'
+	[ "$space" == "nonmetric_SMACOF" ] && baseline_flag='-b data/NOUN/mds/correlations/baseline_vectors.pickle' || baseline_flag=''
+	python -m code.mds.correlations.mds_correlations data/NOUN/mds/similarities/sim.pickle 'data/NOUN/mds/correlations/'"$space"'_distances.pickle' 'data/NOUN/mds/correlations/'"$space"'.csv' --n_max $dims $correlation_metrics -s 42 $vectors_flag $baseline_flag &> 'data/NOUN/mds/correlations/'"$space"'-log.txt' &
 done
 python -m code.mds.correlations.mds_correlations data/NOUN/mds/similarities/sim.pickle 'data/NOUN/mds/correlations/HorstHout_distances.pickle' data/NOUN/mds/correlations/HorstHout.csv --n_min 4 --n_max 4 -v 'data/NOUN/mds/vectors/HorstHout/vectors.pickle' $correlation_metrics -s 42 &> data/NOUN/mds/correlations/HorstHout-log.txt &
 wait
@@ -41,7 +42,7 @@ wait
 echo 'visualizing correlation results'
 # overview graphs
 python -m code.mds.correlations.visualize_pixel_correlations data/NOUN/mds/correlations/pixel.csv data/NOUN/mds/visualizations/correlations/ &> data/NOUN/mds/correlations/best_pixel.txt &
-# scatter plots for nonmetric SMACOF
+# Shepard plots for nonmetric SMACOF
 for i in `seq 1 $max`
 do
 	python -m code.mds.correlations.shepard_diagram data/NOUN/mds/similarities/sim.pickle 'data/NOUN/mds/correlations/nonmetric_SMACOF_distances.pickle' 'data/NOUN/mds/visualizations/correlations/scatter/nonmetric_SMACOF_'"$i"'D.png' --mds $i -d Euclidean &	
@@ -51,7 +52,8 @@ python -m code.mds.correlations.shepard_diagram data/NOUN/mds/similarities/sim.p
 python -m code.mds.correlations.shepard_diagram data/NOUN/mds/similarities/sim.pickle data/NOUN/mds/correlations/ann_distances.pickle data/NOUN/mds/visualizations/correlations/scatter/ANN_optimized.png --ann  -d Euclidean -n 5 -s 42 &
 # Shepard plot for best pixel baseline
 python -m code.mds.correlations.shepard_diagram data/NOUN/mds/similarities/sim.pickle data/NOUN/mds/correlations/pixel_distances/ data/NOUN/mds/visualizations/correlations/scatter/pixel_fixed.png --pixel min -b 18 -d Manhattan  &
-python -m code.mds.correlations.shepard_diagram data/NOUN/mds/similarities/sim.pickle data/NOUN/mds/correlations/pixel_distances/ data/NOUN/mds/visualizations/correlations/scatter/pixel_optimized.png --pixel min -b 2 -d Euclidean -n 5 -s 42 &
+python -m code.mds.correlations.shepard_diagram data/NOUN/mds/similarities/sim.pickle data/NOUN/mds/correlations/pixel_distances/ data/NOUN/mds/visualizations/correlations/scatter/pixel_fixed_optimized.png --pixel min -b 18 -d Manhattan  -n 5 -s 42&
+python -m code.mds.correlations.shepard_diagram data/NOUN/mds/similarities/sim.pickle data/NOUN/mds/correlations/pixel_distances/ data/NOUN/mds/visualizations/correlations/scatter/pixel_optimized.png --pixel max -b 1 -d Euclidean -n 5 -s 42 &
 wait
 
 
