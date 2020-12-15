569368723037b4af7c8d83b9efe21eb5c7e63ad9
Lucas Bechberger <lbechberger@uos.de>
2020-12-15 08:34:51 +0100

    70 instead of 71 additional images (balanced folds)
    removed zebra image since too much black

--- code/shell_scripts/Shapes/ml_setup.sh
+++ code/shell_scripts/Shapes/ml_setup.sh
@@ -11,8 +11,8 @@ echo 'preparing data set for machine learning'
 
 # create more artificial images
 echo '    augmentation'
-python -m code.ml.preprocessing.prepare_Shapes_data data/Shapes/ml/folds/Shapes.csv data/Shapes/ml/dataset/ 4168 -s 42 -p data/Shapes/ml/dataset/pickle/ -n 0.1 0.25 0.55 > data/Shapes/ml/dataset/Shapes_stat.txt
-python -m code.ml.preprocessing.prepare_Shapes_data data/Shapes/ml/folds/Additional.csv data/Shapes/ml/dataset/ 3520 -s 42 > data/Shapes/ml/dataset/Additional_stat.txt
+python -m code.ml.preprocessing.prepare_Shapes_data data/Shapes/ml/folds/Shapes.csv data/Shapes/ml/dataset/ 4166 -s 42 -p data/Shapes/ml/dataset/pickle/ -n 0.1 0.25 0.55 > data/Shapes/ml/dataset/Shapes_stat.txt
+python -m code.ml.preprocessing.prepare_Shapes_data data/Shapes/ml/folds/Additional.csv data/Shapes/ml/dataset/ 3572 -s 42 > data/Shapes/ml/dataset/Additional_stat.txt
 python -m code.ml.preprocessing.prepare_Shapes_data data/Shapes/ml/folds/Berlin.csv data/Shapes/ml/dataset/ 25 -s 42 > data/Shapes/ml/dataset/Berlin_stat.txt
 python -m code.ml.preprocessing.prepare_Shapes_data data/Shapes/ml/folds/Sketchy.csv data/Shapes/ml/dataset/ 8 -s 42 > data/Shapes/ml/dataset/Sketchy_stat.txt
 
