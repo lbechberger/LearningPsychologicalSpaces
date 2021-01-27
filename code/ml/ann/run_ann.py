27cd647f4c40ab7b6fa4afebc481bc1d602dc858
Lucas Bechberger <lbechberger@uos.de>
2021-01-27 12:35:29 +0100

    default Adam configuration

--- code/ml/ann/run_ann.py
+++ code/ml/ann/run_ann.py
@@ -275,7 +275,7 @@ def create_model(do_classification, do_mapping, do_reconstruction):
         
     # set up model, loss, and evaluation metrics
     model = tf.keras.models.Model(inputs = enc_input, outputs = model_outputs)
-    adam = tf.keras.optimizers.Adam(lr=0.01)
+    adam = tf.keras.optimizers.Adam()
     model.compile(optimizer = adam, loss = model_loss, loss_weights = model_loss_weights, weighted_metrics = model_metrics)
     #model.summary()
     
