033eb9f10cff7251f93e3b362a9dee105b17e556
Lucas Bechberger <lbechberger@uos.de>
2021-02-01 16:07:01 +0100

    preparing grid search

--- code/shell_scripts/Shapes/experiment_2.sh
+++ code/shell_scripts/Shapes/experiment_2.sh
@@ -78,11 +78,35 @@ do
 	done
 done
 
-
 # aggregate results for increased convenience
 python -m code.ml.ann.average_folds data/Shapes/ml/experiment_2/vanilla.csv data/Shapes/ml/experiment_2/aggregated/vanilla.csv
 python -m code.ml.ann.average_folds data/Shapes/ml/experiment_2/decay.csv data/Shapes/ml/experiment_2/aggregated/decay.csv
 python -m code.ml.ann.average_folds data/Shapes/ml/experiment_2/dropout.csv data/Shapes/ml/experiment_2/aggregated/dropout.csv
 python -m code.ml.ann.average_folds data/Shapes/ml/experiment_2/noise.csv data/Shapes/ml/experiment_2/aggregated/noise.csv
 python -m code.ml.ann.average_folds data/Shapes/ml/experiment_2/bottleneck.csv data/Shapes/ml/experiment_2/aggregated/bottleneck.csv
+
+
+# grid search on most promising candidates
+echo '-w 0.0005 -e -n 0.25 -b 256' > data/Shapes/ml/experiment_2/grid_search.config
+echo '-w 0.0005 -n 0.1 -b 256' >> data/Shapes/ml/experiment_2/grid_search.config
+echo '-w 0.0005 -n 0.25 -b 512' >> data/Shapes/ml/experiment_2/grid_search.config
+echo '-w 0.0005 -n 0.25 -b 256' >> data/Shapes/ml/experiment_2/grid_search.config
+echo '-w 0.001 -e -n 0.1 -b 256' >> data/Shapes/ml/experiment_2/grid_search.config
+echo '-w 0.001 -e -n 0.25 -b 512' >> data/Shapes/ml/experiment_2/grid_search.config
+echo '-w 0.001 -e -n 0.25 -b 256' >> data/Shapes/ml/experiment_2/grid_search.config
+echo '-w 0.001 -n 0.1 -b 512' >> data/Shapes/ml/experiment_2/grid_search.config
+echo '-w 0.001 -n 0.1 -b 256' >> data/Shapes/ml/experiment_2/grid_search.config
+echo '-w 0.001 -n 0.25 -b 512' >> data/Shapes/ml/experiment_2/grid_search.config
+echo '-w 0.001 -n 0.25 -b 256' >> data/Shapes/ml/experiment_2/grid_search.config
+
+while IFS= read -r params
+do
+	for fold in $folds
+	do
+		$cmd $script data/Shapes/ml/dataset/Shapes.pickle data/Shapes/ml/dataset/Additional.pickle data/Shapes/ml/dataset/Berlin.pickle data/Shapes/ml/dataset/Sketchy.pickle data/Shapes/ml/dataset/targets.pickle mean_4 data/Shapes/images/ data/Shapes/mds/similarities/aggregator/mean/aggregated_ratings.pickle data/Shapes/ml/experiment_2/grid_search.csv -c 1.0 -r 0.0 -m 0.0 -f $fold -s 42 $walltime $params
+	done
+done < 'data/Shapes/ml/experiment_2/grid_search.config'
+
+# aggregate results for increased convenience
+python -m code.ml.ann.average_folds data/Shapes/ml/experiment_2/grid_search.csv data/Shapes/ml/experiment_2/aggregated/grid_search.csv
 
