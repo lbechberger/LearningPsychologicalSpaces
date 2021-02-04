d91dc063cc36d4ce4d9d75f26978e08536096c55
Lucas Bechberger <lbechberger@uos.de>
2021-02-04 11:30:52 +0100

    temporary script for starting the regression jobs

--- code/shell_scripts/Shapes/tmp.sh
+++ code/shell_scripts/Shapes/tmp.sh
@@ -31,46 +31,22 @@ else
 	exit 1
 fi
 
-# set up the directory structure
-echo '    setting up directory structure'
-mkdir -p 'data/Shapes/ml/experiment_3/features' 'data/Shapes/ml/experiment_3/aggregated'
-
-
-# define snapshots of default classifier
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.0005_v0.0_eTrue_dFalse_n0.1_mean_4_f0_ep25_FINAL.h5 data/Shapes/ml/experiment_3/features/default_f0.pickle' > data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.0005_v0.0_eTrue_dFalse_n0.1_mean_4_f1_ep21_FINAL.h5 data/Shapes/ml/experiment_3/features/default_f1.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.0005_v0.0_eTrue_dFalse_n0.1_mean_4_f2_ep55_FINAL.h5 data/Shapes/ml/experiment_3/features/default_f2.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.0005_v0.0_eTrue_dFalse_n0.1_mean_4_f3_ep28_FINAL.h5 data/Shapes/ml/experiment_3/features/default_f3.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.0005_v0.0_eTrue_dFalse_n0.1_mean_4_f4_ep31_FINAL.h5 data/Shapes/ml/experiment_3/features/default_f4.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-
-# define snapshots of classifier with highest accuracy
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.001_v0.0_eTrue_dFalse_n0.1_mean_4_f0_ep51_FINAL.h5 data/Shapes/ml/experiment_3/features/accuracy_f0.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.001_v0.0_eTrue_dFalse_n0.1_mean_4_f1_ep71_FINAL.h5 data/Shapes/ml/experiment_3/features/accuracy_f1.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.001_v0.0_eTrue_dFalse_n0.1_mean_4_f2_ep47_FINAL.h5 data/Shapes/ml/experiment_3/features/accuracy_f2.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.001_v0.0_eTrue_dFalse_n0.1_mean_4_f3_ep92_FINAL.h5 data/Shapes/ml/experiment_3/features/accuracy_f3.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.001_v0.0_eTrue_dFalse_n0.1_mean_4_f4_ep51_FINAL.h5 data/Shapes/ml/experiment_3/features/accuracy_f4.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-
-# define snapshots of classifier with highest correlation
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.001_v0.0_eTrue_dFalse_n0.25_mean_4_f0_ep90_FINAL.h5 data/Shapes/ml/experiment_3/features/correlation_f0.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.001_v0.0_eTrue_dFalse_n0.25_mean_4_f1_ep85_FINAL.h5 data/Shapes/ml/experiment_3/features/correlation_f1.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.001_v0.0_eTrue_dFalse_n0.25_mean_4_f2_ep86_FINAL.h5 data/Shapes/ml/experiment_3/features/correlation_f2.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.001_v0.0_eTrue_dFalse_n0.25_mean_4_f3_ep95_FINAL.h5 data/Shapes/ml/experiment_3/features/correlation_f3.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b512_w0.001_v0.0_eTrue_dFalse_n0.25_mean_4_f4_ep83_FINAL.h5 data/Shapes/ml/experiment_3/features/correlation_f4.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-
-# define snapshots of classifier with best results for small bottleneck
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b64_w0.001_v0.0_eTrue_dFalse_n0.1_mean_4_f0_ep96_FINAL.h5 data/Shapes/ml/experiment_3/features/small_f0.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b64_w0.001_v0.0_eTrue_dFalse_n0.1_mean_4_f1_ep88_FINAL.h5 data/Shapes/ml/experiment_3/features/small_f1.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b64_w0.001_v0.0_eTrue_dFalse_n0.1_mean_4_f2_ep96_FINAL.h5 data/Shapes/ml/experiment_3/features/small_f2.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b64_w0.001_v0.0_eTrue_dFalse_n0.1_mean_4_f3_ep93_FINAL.h5 data/Shapes/ml/experiment_3/features/small_f3.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-echo 'data/Shapes/ml/snapshots/c1.0_r0.0_m0.0_b64_w0.001_v0.0_eTrue_dFalse_n0.1_mean_4_f4_ep95_FINAL.h5 data/Shapes/ml/experiment_3/features/small_f4.pickle' >> data/Shapes/ml/experiment_3/snapshots.config
-
-
-# extract all the features
-while IFS= read -r config 
+# run the regression
+for feature in $features
 do
-	$cmd $bottleneck_script data/Shapes/ml/dataset/Shapes.pickle $config
-done < 'data/Shapes/ml/experiment_3/snapshots.config'
+	for fold in $folds
+	do
+		for regressor in $regressors
+		do
+			$cmd $regression_script data/Shapes/ml/dataset/targets.pickle mean_4 'data/Shapes/ml/experiment_3/features/'"$feature"'_f'"$fold"'.pickle' data/Shapes/ml/dataset/pickle/folds.csv 'data/Shapes/ml/experiment_3/'"$feature"'_f'"$fold"'.csv' -s 42 $regressor
+		done
 
+		for lasso in $lassos
+		do
+			$cmd $regression_script data/Shapes/ml/dataset/targets.pickle mean_4 'data/Shapes/ml/experiment_3/features/'"$feature"'_f'"$fold"'.pickle' data/Shapes/ml/dataset/pickle/folds.csv 'data/Shapes/ml/experiment_3/'"$feature"'_f'"$fold"'.csv' -s 42 --lasso $lasso
+		done
 
+	done
+done
 
 
