c7ff045348f6f0242de5016898fc56091781e096
Lucas Bechberger <lbechberger@uos.de>
2021-04-12 13:27:36 +0200

    include inception in experiment_5

--- code/shell_scripts/Shapes/experiment_5.sh
+++ code/shell_scripts/Shapes/experiment_5.sh
@@ -34,7 +34,7 @@ fi
 
 # set up the directory structure
 echo '    setting up directory structure'
-mkdir -p 'data/Shapes/ml/experiment_5/logs/' 'data/Shapes/ml/experiment_5/snapshots/' 'data/Shapes/ml/experiment_5/aggregated' 'data/Shapes/ml/experiment_5/raw'
+mkdir -p 'data/Shapes/ml/experiment_5/logs/' 'data/Shapes/ml/experiment_5/snapshots/' 'data/Shapes/ml/experiment_5/aggregated' 'data/Shapes/ml/experiment_5/transfer' 'data/Shapes/ml/experiment_5/inception'
 
 
 # start ann configurations (multi-task learning)
@@ -47,12 +47,18 @@ do
 done
 
 
+# run lasso regression (inception)
+for dim in $dims
+do
+	$cmd $regression_script data/Shapes/ml/dataset/targets.pickle 'mean_'"$dim" 'data/Shapes/ml/dataset/pickle/features_0.1.pickle' data/Shapes/ml/dataset/pickle/folds.csv 'data/Shapes/ml/experiment_5/inception/mean_'"$dim"'.csv' -s 42 -e data/Shapes/ml/dataset/pickle/features_0.0.pickle --lasso 0.005
+done
+
 # run lasso regression (transfer learning)
 for dim in $dims
 do
 	for fold in $folds
 	do
-		$cmd $regression_script data/Shapes/ml/dataset/targets.pickle 'mean_'"$dim" 'data/Shapes/ml/experiment_3/features/small_f'"$fold"'_noisy.pickle' data/Shapes/ml/dataset/pickle/folds.csv 'data/Shapes/ml/experiment_5/raw/mean_'"$dim"'_f'"$fold"'.csv' -s 42 -e 'data/Shapes/ml/experiment_3/features/small_f'"$fold"'_clean.pickle' --lasso 0.02
+		$cmd $regression_script data/Shapes/ml/dataset/targets.pickle 'mean_'"$dim" 'data/Shapes/ml/experiment_3/features/small_f'"$fold"'_noisy.pickle' data/Shapes/ml/dataset/pickle/folds.csv 'data/Shapes/ml/experiment_5/transfer/mean_'"$dim"'_f'"$fold"'.csv' -s 42 -e 'data/Shapes/ml/experiment_3/features/small_f'"$fold"'_clean.pickle' --lasso 0.02
 	done
 done
 
@@ -61,6 +67,6 @@ done
 python -m code.ml.ann.average_folds data/Shapes/ml/experiment_5/multi-task.csv data/Shapes/ml/experiment_5/aggregated/multi-task.csv
 for dim in $dims
 do
-	python -m code.ml.regression.average_folds 'data/Shapes/ml/experiment_5/raw/mean_'"$dim"'_f{0}.csv' 5 'data/Shapes/ml/experiment_5/aggregated/transfer_mean_'"$dim"'.csv'
+	python -m code.ml.regression.average_folds 'data/Shapes/ml/experiment_5/transfer/mean_'"$dim"'_f{0}.csv' 5 'data/Shapes/ml/experiment_5/aggregated/transfer_mean_'"$dim"'.csv'
 done
 
