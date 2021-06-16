c6e61a3e087b3c8cea609fe12c27472440a71cd0
Lucas Bechberger <lbechberger@uos.de>
2021-06-16 10:33:15 +0200

    setup for transfer learning on autoencoder

--- code/shell_scripts/Shapes/tmp.sh
+++ code/shell_scripts/Shapes/tmp.sh
@@ -1,52 +1,71 @@
 #!/bin/bash
 
-echo 'experiment 6 - reconstruction baseline'
+echo 'experiment 7 - regression on top of autoencoder'
 
 # setting up overall variables
 default_folds=("0 1 2 3 4")
-default_weight_decays_enc=("0.0 0.0002 0.001 0.002")
-default_weight_decays_dec=("0.0002 0.0005 0.001 0.002")
-default_noises=("0.0 0.25 0.55")
-default_bottlenecks=("2048 256 128 64 32 16")
+default_regressors=("--linear")
+default_lassos=("0.001 0.002 0.005 0.01 0.02 0.05 0.1 0.2 0.5 1.0 2.0 5.0 10.0")
+default_features=("default best")
+default_noises=("noisy clean")
 default_image_size=224
-default_epochs=200
-default_patience=200
-default_reconstruction_seeds=("0 42 1337 123456")
-default_reconstruction_noises=("0.0 0.1 0.25 0.55")
-
 
 folds="${folds:-$default_folds}"
-weight_decays_enc="${weight_decays:-$default_weight_decays_enc}"
-weight_decays_dec="${weight_decays_dec:-$default_weight_decays_dec}"
-noises="${noises:-$default_noises}"
-bottlenecks="${bottlenecks:-$default_bottlenecks}"
+regressors="${regressors:-$default_regressors}"
+lassos="${lassos:-$default_lassos}"
+features="${features_exp7:-$default_features}"
+noises="${noises_exp7:-$default_noises}"
 image_size="${image_size:-$default_image_size}"
-epochs="${epochs:-$default_epochs}"
-patience="${patience:-$default_patience}"
-reconstruction_seeds="${reconstruction_seeds:-$default_reconstruction_seeds}"
-reconstruction_noises="${reconstruction_noises:-$default_reconstruction_noises}"
 
-# visualize reconstructions
+# no parameter means local execution
+if [ "$#" -ne 1 ]
+then
+	echo '[local execution]'
+	cmd='python -m'
+	bottleneck_script=code.ml.ann.get_bottleneck_activations
+	regression_script=code.ml.regression.regression
+# parameter 'grid' means execution on grid
+elif [ $1 = grid ]
+then
+	echo '[grid execution]'
+	cmd=qsub
+	bottleneck_script=code/ml/ann/get_bottleneck_activations.sge
+	regression_script=code/ml/regression/regression.sge
+# all other parameters are not supported
+else
+	echo '[ERROR: argument not supported, exiting now!]'
+	exit 1
+fi
 
-declare -a configs=(
-	"default data/Shapes/ml/experiment_6/snapshots/c0.0_r1.0_m0.0_b512_w0.0005_v0.0_eTrue_dFalse_n0.1_mean_4_f2_ep48_FINAL.h5"
-	"best data/Shapes/ml/experiment_6/snapshots/c0.0_r1.0_m0.0_b512_w0.0_v0.0_eFalse_dFalse_n0.1_mean_4_f2_ep175_FINAL.h5"
-)
-
-for seed in $reconstruction_seeds
+# run the regression
+for feature in $features
 do
-	python -m code.ml.ann.visualize_reconstruction data/Shapes/ml/experiment_6/snapshots/c0.0_r1.0_m0.0_b512_w0.0_v0.0_eFalse_dFalse_n0.1_mean_4_f2_ep175_FINAL.h5 data/Shapes/images/C21I07_parrot.png 'data/Shapes/ml/experiment_6/images/best-n0.1-s'"$seed"'.png' -i $image_size -s $seed -n 0.1 
+	for fold in $folds
+	do
+		for regressor in $regressors
+		do
+			$cmd $regression_script data/Shapes/ml/dataset/targets.pickle mean_4 'data/Shapes/ml/experiment_7/features/'"$feature"'_f'"$fold"'_noisy.pickle' data/Shapes/ml/dataset/pickle/folds.csv 'data/Shapes/ml/experiment_7/'"$feature"'_f'"$fold"'.csv' -s 42 -e 'data/Shapes/ml/experiment_7/features/'"$feature"'_f'"$fold"'_clean.pickle' $regressor
+		done
+
+		for lasso in $lassos
+		do
+			$cmd $regression_script data/Shapes/ml/dataset/targets.pickle mean_4 'data/Shapes/ml/experiment_7/features/'"$feature"'_f'"$fold"'_noisy.pickle' data/Shapes/ml/dataset/pickle/folds.csv 'data/Shapes/ml/experiment_7/'"$feature"'_f'"$fold"'.csv' -s 42 -e 'data/Shapes/ml/experiment_7/features/'"$feature"'_f'"$fold"'_clean.pickle' --lasso $lasso
+		done
+
+	done
 done
 
-for noise in $reconstruction_noises
-do
-	python -m code.ml.ann.visualize_reconstruction data/Shapes/ml/experiment_6/snapshots/c0.0_r1.0_m0.0_b512_w0.0_v0.0_eFalse_dFalse_n0.1_mean_4_f2_ep175_FINAL.h5 data/Shapes/images/C21I07_parrot.png 'data/Shapes/ml/experiment_6/images/best-n'"$noise"'-s42.png' -i $image_size -s 42 -n $noise
-done
 
-for config in "${configs[@]}"
-do
-	read -a elements <<< "$config"
-	python -m code.ml.ann.visualize_reconstruction ${elements[1]} data/Shapes/images/C21I07_parrot.png 'data/Shapes/ml/experiment_6/images/'"${elements[0]}"'-n0.1-s42.png' -i $image_size -s 42 -n 0.1
-done
 
+# do a cluster analysis
+for feature in $features
+do
+	for fold in $folds
+	do
+		for noise in $noises
+		do
+			python -m code.ml.regression.cluster_analysis 'data/Shapes/ml/experiment_7/features/'"$feature"'_f'"$fold"'_'"$noise"'.pickle' -n 100 -s 42 > 'data/Shapes/ml/experiment_7/features/'"$feature"'_f'"$fold"'_'"$noise"'.txt'
+		done
+	done
+done
 
