0f8b02a7329e48fd152dd5c40d8cd103b73ccca6
lbechberger <lbechberger@uos.de>
2022-05-25 15:02:28 +0200

    hardcoded analysis of similarity ratings

--- code/mds/data_analysis/analyze_visual_conceptual.r
+++ code/mds/data_analysis/analyze_visual_conceptual.r
@@ -10,41 +10,12 @@ if(!require(ordinal)){
   install.packages("ordinal", repos = "http://cran.us.r-project.org")
   library(ordinal)
 }
-## for ANOVA
-#if(!require(car)){
-#  install.packages("car", repos = "http://cran.us.r-project.org")
-#  library(car)
-#}
-## for ANOVA
-#if(!require(FactoMineR)){
-#  install.packages("FactoMineR", repos = "http://cran.us.r-project.org")
-#  library(FactoMineR)
-#}
-## for ANOVA
-#if (!requireNamespace("BiocManager", quietly = TRUE))
-#  install.packages("BiocManager", repos = "http://cran.us.r-project.org")
-#if(!require(mixOmics)){
-#  BiocManager::install("mixOmics")
-#}
-## for ANOVA
-#if(!require(RVAideMemoire)){
-#  install.packages("RVAideMemoire", repos = "http://cran.us.r-project.org")
-#  library(RVAideMemoire)
-#}
-## for t test
-#if(!require(pastecs)){
-#  install.packages("pastecs", repos = "http://cran.us.r-project.org")
-#  library(pastecs)
-#}
-## for plots
-#if(!require(ggplot2)){
-#  install.packages("ggplot2", repos = "http://cran.us.r-project.org")
-#  library(ggplot2)
-#}
 
 # parse the command line arguments
 option_list = list(make_option(c("-c", "--conceptual_file"), type = "character", default = NULL, help = "path to input file with conceptual ratings"),
-                   make_option(c("-v", "--visual_file"), type = "character", default = NULL, help = "path to input file with visual ratings"))
+                   make_option(c("-v", "--visual_file"), type = "character", default = NULL, help = "path to input file with visual ratings"),
+                   make_option(c("-t", "--test_assumptions"), action = "store_true", default = FALSE, help = "test CLMM assumptions"),
+                   make_option(c("--verbose"), action = "store_true", default = FALSE, help = "output all models"))
 
 opt_parser = OptionParser(option_list = option_list)
 opt = parse_args(opt_parser)
@@ -63,52 +34,165 @@ data <- rbind(data_conceptual, data_visual)
 data$ratings <- factor(data$ratings, ordered=T)
 data$pairID <-as.factor(data$pairID)
 
-# helper function for running clmm on conceptual vs visual on a given data table
-run_clmm <- function(input_data) {
-  #str(input_data)
-  
-  # build mixed effects model (CLMM): 
-  #   do ratings depend on rating type? --> ratings ~ ratingType
-  #   taking into account that individual picture pair may influence rating --> (1|pairID)
-  #   taking into account that difference between rating types may differ among picture pairs --> (1 + ratingType | pairID)
-  print("fitting clmm")
-  model <<- clmm(ratings ~ ratingType + (1+ratingType|pairID),  data=input_data)
-  print(model)
+
+
+# ------------------------------------------------------------------------------
+# first look at the full dataset and investigate influence of ratingType
+
+print("CLMM analysis on full data")
+print("--------------------------")
+print("  Fitting alternative hypothesis (main effect of ratingType)")
+# build mixed effects model (CLMM): 
+#   do ratings depend on rating type? --> ratings ~ ratingType
+#   taking into account that individual picture pair may influence rating --> (1|pairID)
+#   taking into account that difference between rating types may differ among picture pairs --> (1 + ratingType | pairID)
+model.all <<- clmm(ratings ~ ratingType + (1+ratingType|pairID),  data = data)
+if(opt$verbose) {
+  print(model.all)
   print('')
-  
-  # test model assumptions:
-#  print("fitting clm for testing model assumptions")
-#  model.test <- clm(ratings ~ ratingType + pairID, data=input_data)
-#  print(model.test)
-#  # first: proportional odds/equal slopes
-#  print(nominal_test(model.test))
-#  print('')
-#  
-#  # second: scale effects 
-#  print(scale_test(model.test))
-#  print('')
-  
-  # check significance of ratingType effect
-  #   null hypothesis: ratingType does not matter
-  model.null <<- clmm(ratings ~ 1 + (1+ratingType|pairID), data=input_data, control=clmm.control(gradTol = 3e-4))
-  print(model.null)
-  print('')
-  
-  print(anova(model, model.null))
-  print('')
-  
-  # TODO: check significance of visualType effect and interaction of effects
 }
 
-# TODO: call helper function on all data, within pairs, between pairs
-run_clmm(data)
 
+# test model assumptions if user wants us to (computationally very expensive!)
+if (opt$test_assumptions) {
+  print("  Testing model assumptions with CLM...")
+  
+  model.all.test <- clm(ratings ~ ratingType + pairID, data = data)
+  
+  # first: proportional odds/equal slopes
+  print(nominal_test(model.all.test))
+  print('')
+  
+  # second: scale effects 
+  print(scale_test(model.all.test))
+  print('')
+}
+
+# null hypothesis: ratingType does not matter
+print("  Fitting null hypothesis (no effect of ratingType)")
+model.all.null <<- clmm(ratings ~ 1 + (1+ratingType|pairID), data = data, control=clmm.control(gradTol = 3e-4))
+if(opt$verbose) {
+  print(model.all.null)
+  print('')
+}
+
+# conducting likelihood ratio test
+print("  Likelihood ratio test...")
+print(anova(model.all, model.all.null))
+print('')
+
+
+
+
+#-------------------------------------------------------------------------------
+# look at between-category ratings and see whether the ratingType effect persists
+
+print("CLMM analysis on between-category data")
+print("--------------------------------------")
+print("  Fitting alternative hypothesis (main effect of ratingType)")
 between <- subset(data, pairType=="between")
-#run_clmm(between)
+# same model as above, but only subset of data
+model.between <<- clmm(ratings ~ ratingType + (1+ratingType|pairID),  data = between)
+if(opt$verbose) {
+  print(model.between)
+  print('')
+}
 
+# test model assumptions if user wants us to (computationally very expensive!)
+if (opt$test_assumptions) {
+  print("  Testing model assumptions with CLM...")
+  
+  model.between.test <- clm(ratings ~ ratingType + pairID, data = between)
+  
+  # first: proportional odds/equal slopes
+  print(nominal_test(model.between.test))
+  print('')
+  
+  # second: scale effects 
+  print(scale_test(model.between.test))
+  print('')
+}
+
+
+print("  Fitting null hypothesis (no effect of ratingType)")
+# same null hypothesis as above, but only subset of data
+model.between.null <<- clmm(ratings ~ 1 + (1+ratingType|pairID), data=between, control=clmm.control(gradTol = 3e-4))
+if(opt$verbose) {
+  print(model.between.null)
+  print('')
+}
+
+# conducting likelihood ratio test
+print("  Likelihood ratio test...")
+print(anova(model.between, model.between.null))
+print('')
+
+
+
+
+#-------------------------------------------------------------------------------
+# now look at within-category ratings, investigating effects of both ratingType and visualType
+print("CLMM analysis on within-category data")
+print("--------------------------------------")
+print("  Fitting alternative hypothesis (interaction betwen ratingType and visualType)")
 within <- subset(data, pairType=="within")
-#run_clmm(within)
+# essentially same model, but also considering visualType of the category (VC/VV) as predictor
+#   "ratingType * visualType" also allows for interaction of the two predictors
+model.within <<- clmm(ratings ~ ratingType * visualType + (1+ratingType|pairID),  data = within)
+if(opt$verbose) {
+  print(model.within)
+  print('')
+}
 
-# TODO ratingType and visualType effects
+# test model assumptions if user wants us to (computationally very expensive!)
+if (opt$test_assumptions) {
+  print("  Testing model assumptions with CLM...")
+  
+  model.within.test <- clm(ratings ~ ratingType * visualType + pairID, data = within)
+  
+  # first: proportional odds/equal slopes
+  print(nominal_test(model.within.test))
+  print('')
+  
+  # second: scale effects 
+  print(scale_test(model.within.test))
+  print('')
+}
 
-# TODO: spearman correlation of visual vs conceptual ratings
+# first null hypothesis: no interaction of ratingType and visualType
+print("  Fitting null hypothesis (no interaction of ratingType and visualType)")
+model.within.null.noInteraction <<- clmm(ratings ~ ratingType + visualType + (1+ratingType|pairID), data = within)
+if(opt$verbose) {
+  print(model.within.null.ratingType)
+  print('')
+}
+
+print("  Likelihood ratio test...")
+print(anova(model.within, model.within.null.noInteraction))
+print('')
+
+# second null hypothesis: no main effect of ratingType
+print("  Fitting null hypothesis (no effect of ratingType)")
+model.within.null.ratingType <<- clmm(ratings ~ visualType + (1+ratingType|pairID), data=within, control=clmm.control(gradTol = 3e-4))
+if(opt$verbose) {
+  print(model.within.null.ratingType)
+  print('')
+}
+
+# conducting likelihood ratio test
+print("  Likelihood ratio test...")
+print(anova(model.within, model.within.null.ratingType))
+print('')
+
+# third null hypothesis: no main effect of visualType
+print("  Fitting null hypothesis (no effect of visualType)")
+model.within.null.visualType <<- clmm(ratings ~ ratingType + (1+ratingType|pairID), data=within, control=clmm.control(gradTol = 3e-4))
+if(opt$verbose) {
+  print(model.within.null.visualType)
+  print('')
+}
+
+# conducting likelihood ratio test
+print("  Likelihood ratio test...")
+print(anova(model.within, model.within.null.visualType))
+print('')
